75ce74f66bbc3b1d4a3e779df53df3e89cd69dec
Christopher Wood <christopherwood07@gmail.com>
2016-01-04 10:05:16 -0800

    home server checkin

---
+++ home/home/home.go
@@ -0,0 +1,52 @@
+package hello
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "log"
+
+    // "google.golang.org/appengine"
+    // "google.golang.org/appengine/log"
+)
+
+type SoundReading struct {
+    Time int `json:"time"`
+    Value int `json:"value"`
+}
+
+var soundReadings []SoundReading;
+
+func init() {
+    soundReadings = make([]SoundReading, 100) // 100 to start, it grows underneath
+
+    http.HandleFunc("/", homeHandler)
+    http.HandleFunc("/sound", soundHandler)
+}
+
+// curl -H "Content-Type: application/json" -X POST -d '{"time": 100, "value": 120}' http://localhost:8080/sound
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Welcome home.")
+}
+
+func soundHandler(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+        case "GET": // Serve the resource.
+            // TODO!
+            break
+        case "POST": // Create a new record.
+            decoder := json.NewDecoder(r.Body)
+            var reading SoundReading
+            err := decoder.Decode(&reading)
+            if err != nil {
+                fmt.Println(err);
+            } else {
+                soundReadings = append(soundReadings, reading)
+            }
+
+            // c := appengine.NewContext(r)
+            log.Printf("Saving value: %v\n", reading)
+        default:
+            // Give an error message.
+    }
+}
